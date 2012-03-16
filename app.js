9d74f0d5b3155518d6840bf4e0c7603dfabcd565
Laurent Bédubourg <laurent@labe.me>
2012-03-16 14:40:26 +0100

    Blah

--- app.js
+++ app.js
@@ -8,3 +8,4 @@ if (config.store.type == "redis"){
 var auth = new diemids.Authorizer(store);
 var deliverer = new diemids.Deliverer(config, auth);
 var backend = new diemids.Backend(config, auth);
+console.log("Deliver It Enthusiastic! My Indie Delivery Service is running.");
