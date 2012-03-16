84d433e97bd7d7aaf466a9771745a4138b8365de
Laurent Bédubourg <laurent@labe.me>
2012-03-16 14:36:40 +0100

    Cleaning up configuration

--- Makefile
+++ Makefile
@@ -1,6 +1,2 @@
 start-server:
-	PEM_FILE=/Users/lbedubourg/aws-cloudfront-pk-APKAJD7NA5SRFY5XMTKA.pem \
-	KEY_PAIR_ID=APKAJD7NA5SRFY5XMTKA \
-	CLOUDFRONT_DOMAIN=http://d6ip2vu15e2u2.cloudfront.net \
-    BACKEND_SECRET=example \
 	node app.js
--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var config = require('./config');
 var diemids = require("./lib");
-var redis = require('redis').createClient();
-var store = new diemids.RedisStore(redis);
+var store = null;
+if (config.store.type == "redis"){
+    var redis = require('redis').createClient();
+    store = new diemids.RedisStore(redis);
+}
 var auth = new diemids.Authorizer(store);
 var deliverer = new diemids.Deliverer(config, auth);
 var backend = new diemids.Backend(config, auth);
